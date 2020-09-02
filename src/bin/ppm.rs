96d953b3d7e462282b577f73fe1a7a43143a3030
Elliott Barcikowski <ebarcikowski@gmail.com>
2020-09-01 21:37:41 -0600

    Restructured to what I think is a more normal layout.

--- src/bin/ppm.rs
+++ src/bin/ppm.rs
@@ -1,3 +1,7 @@
+extern crate rtiow;
+
+use rtiow::Vec3;
+
 fn main() {
     let width = 256;
     let height = 256;
@@ -6,6 +10,9 @@ fn main() {
     for i in (0..height).rev() {
         eprintln!("Scanlines remaining: {}", i);
         for j in 0..width {
+
+            let v:Vec3 = Vec3 {x:1.0, y:1.0, z:1.0};
+
             let r = (j as f32) / (width as f32 - 1.0);
             let g = (i as f32) / (height as f32 - 1.0);
             let b:f32 = 0.25;
